027fa5c57af8afe26e67beab93d3f8fa99b81f74
Sorin <sorin.biriescu@gmail.com>
2017-10-18 20:57:51 +0200

    Added bootstrap template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
