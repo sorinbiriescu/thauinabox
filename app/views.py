deea595dafbb50fb51bb1832d1ea1052a85ecfee
Sorin <sorin.biriescu@gmail.com>
2017-10-24 22:25:38 +0200

    Added timeline page + translated the menu in french

--- app/views.py
+++ app/views.py
@@ -5,5 +5,9 @@ from flask import render_template, request, redirect, url_for
 @app.route('/index')
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route("/timeline")
+def timeline():
+    return render_template('timeline.html')
