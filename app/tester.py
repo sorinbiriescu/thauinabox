ca75f10c001690ccf5b0bc44602c6e06c81b17dd
Sorin <sorin.biriescu@gmail.com>
2017-11-06 23:15:58 +0100

    Finished the resume structure with languages, competences etc.

--- app/tester.py
+++ app/tester.py
@@ -3,9 +3,22 @@ import os
 
 script_dir = os.path.dirname(__file__)
 
-work_experience_json = open(os.path.join(script_dir, 'static/json/work_experience.json'),'r')
+work_experience_json = open(os.path.join(script_dir, 'static/json/resume.json'),'r')
 jobs = json.loads(work_experience_json.read())
 
-for i in jobs:
-    print(jobs[i]['job_title'])
+competences = jobs['competences']
+print(competences)
 
+competence_categories = {}
+for k,v in competences.items():
+    if v['type'] in competence_categories.keys():
+        if v['category'] in competence_categories[v['type']]:
+            continue
+        else:
+            competence_categories[v['type']].append(v['category'])
+    else:
+        competence_categories[v['type']] = []
+        competence_categories[v['type']].append(v['category'])
+
+for k,v in competence_categories.items():
+    print(k,v)
--- app/views.py
+++ app/views.py
@@ -16,7 +16,31 @@ def main():
 def resume():
     resume_json = open(os.path.join(script_dir, 'static/json/resume.json'),'r')
     resume_content = json.loads(resume_json.read().encode('utf-8'))
-    return render_template('resume.html',jobs=resume_content['work_experience'])
+    jobs=resume_content['work_experience']
+    education = resume_content['education']
+    languages = resume_content['languages']
+    competences = resume_content['competences']
+    
+    competence_categories = {}
+    for k,v in competences.items():
+        if v['type'] in competence_categories.keys():
+            if v['category'] in competence_categories[v['type']]:
+                continue
+            else:
+                competence_categories[v['type']].append(v['category'])
+        else:
+            competence_categories[v['type']] = []
+            competence_categories[v['type']].append(v['category'])
+
+    content = {
+        "jobs":jobs,
+        "education":education,
+        "languages":languages,
+        "competences":competences,
+        "competence_categories":competence_categories
+    }  
+
+    return render_template('resume.html', ** content)
 
 @app.route("/leisure")
 def leisure():
